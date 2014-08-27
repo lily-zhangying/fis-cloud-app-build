1838484c6b2ba1be32623f872c3ef72107ad9d9b
lily-zhangying <zhangying3210@gmail.com>
2014-08-27 13:18:24 +0800

    add files

---
+++ action/build.js
@@ -0,0 +1,21 @@
+// build.js
+// 0. 找到build的tmp目录
+// 0. task id (svn + time + user)
+
+// shell 
+
+// 1. svn co => task is dir
+// 2. task_id dir build
+// 3. tar output => 产出目录下
+// 4. rm -rf svn dir
+// 5. 返回tar包地址
+
+// url ： http://fedev.baidu.com：8888/build?git=""&user=""&pwd=""&type=0&async=
+//     0: fisp默认
+    // 1: fis默认
+    // 2: build.sh
+
+    //有async返回taskid。
+
+dd
+
